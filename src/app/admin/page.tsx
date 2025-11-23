b0697c0bc75ecf859aba4779ba149424e7d37fdf
Satindar <73517520+Satindar31@users.noreply.github.com>
2025-11-23 16:57:07 +0530

    password protect admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,49 @@
-import AdminForm from "@/components/adminForm";
-import { Suspense } from "react";
+"use client";
 
-export default async function AdminPage() {
-  
+import AdminForm from "@/components/adminForm";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { useState } from "react";
+
+export default function AdminPage() {
+  const [pwd, setPwd] = useState("");
+  const [accessGranted, setAccessGranted] = useState(false);
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log("Submitted OTP:", pwd);
+    if (pwd === process.env.NEXT_PUBLIC_AP) {
+      setAccessGranted(true);
+    } else {
+      alert("Incorrect Password");
+    }
+  }
+
   return (
     <div className="dark bg-[#00030c] text-white min-h-screen flex flex-col gap-4 items-center justify-center">
       <h1 className="text-3xl">Admin Page</h1>
-      <Suspense>
+      {accessGranted ? (
         <AdminForm />
-      </Suspense>
+      ) : (
+        <form onSubmit={handleSubmit}>
+        <InputOTP
+          maxLength={6}
+          value={pwd}
+          onChange={setPwd}
+        >
+          <InputOTPGroup>
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+            <InputOTPSlot index={3} />
+            <InputOTPSlot index={4} />
+            <InputOTPSlot index={5} />
+          </InputOTPGroup>
+        </InputOTP>
+        </form>
+      )}
     </div>
   );
 }
